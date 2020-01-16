4b15b0b9f342cc40566441e663ce9aa0acb00059
raulb98 <52500444+raulb98@users.noreply.github.com>
2020-01-16 20:24:01 +0200

    Update Lease.py

--- Lease.py
+++ Lease.py
@@ -11,6 +11,7 @@ class lease():
         self.start_time = 0
         self.time_to_reconfigure = time_to_reconfigure
         self.half_time = time_to_reconfigure / 2
+        self.rebind_time = 7 * time_to_reconfigure / 8
         self.release_time = time_to_reconfigure
         self.socket = socket
         self.request = req
@@ -24,21 +25,31 @@ class lease():
     def run(self):
         logging.warning('Lease Time is ' + str(self.time_to_reconfigure))
         logging.warning('Lease time started')
-        while self.end_process:
+        while not self.end_process:
             logging.warning('')
             time.sleep(1)
             self.start_time += 1
+            print('Lease time remaining: ' + str(self.start_time))
             
-            if self.start_time >= self.half_time:
+            if self.start_time >= self.half_time and self.start_time < self.rebind_time:
                 my_sender = MessageType.SendMessage(self.request,self.socket)
                 my_sender.sendTo()
-                self.reconfigure()
-                logging.warning('Lease time renewed')
+                msg_rcv = MessageType.RecvMessage(self.socket)
+                if len(msg_rcv) > 0:
+                    self.reconfigure()
+                    logging.warning('Lease time renewed')
             
-            if self.start_time == self.release_time:
-                my_sender = MessageType.SendMessage(Concat_dict.concat_dict(Messages.dhcp_release))
-                my_sender.sendTo()
+            if self.start_time >= self.release_time:
                 self.end_process = True
                 logging.warning('Lease time released')
 
-            print(self.start_time)
+            if self.start_time >= self.rebind_time:
+                assign_ciaddr(Messages.dhcp_request_renew,self.com_level.my_ip_bytes[-1])
+                my_sender = MessageType.SendMessage(Concat_dict.concat_dict(Messages.dhcp_request_renew))
+                my_sender.sendTo()
+                msg_rcv = MessageType.RecvMessage(self.socket)
+                if len(msg_rcv) > 0:
+                    self.reconfigure()
+                    logging.warning('Lease time renewed')
+
+            
